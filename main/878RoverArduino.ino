39dfcd809c7c24cb982b0b324d8d472b4153baf4
Suhail Moeen <suhailmmoeen@gmail.com>
2024-01-20 20:10:22 -0500

    Digital --> Analog Changes

--- main/878RoverArduino.ino
+++ main/878RoverArduino.ino
@@ -1,6 +1,5 @@
 const int analogIpt1 = A0; // infrared input left side
-const int analogIpt2 = A1; // infrared input right side
-const int speed = 100; 
+const int speed = 255; 
 
 // Motor A pins
 const int enableA = 11;
@@ -15,7 +14,6 @@ const int IN4 = 6;
 void setup() {
   // Initialize pin mode
   pinMode(analogIpt1, INPUT);
-  pinMode(analogIpt2, INPUT);
   pinMode(enableA, OUTPUT);
   pinMode(enableB, OUTPUT);
   pinMode(IN1, OUTPUT);
@@ -39,24 +37,41 @@ void controlDir(){
   analogWrite(enableA, speed);
   analogWrite(enableB, speed);
 
+
   digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
+  digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
+  digitalWrite(IN4, LOW);
 
   delay(2000);
 
   digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW;
+  digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN4, HIGH);
 
   delay(2000);
 }
 
 void loop() {
-  changeVal = map(analogRead(analogIpt), 0, 1023, 0, 100);
+  changeVal = map(analogRead(analogIpt1), 0, 1023, 0, 100);
   Serial.print("Detected value: "+ changeVal);
-  controlDir();
-}
 
+  analogWrite(enableA, speed);
+  analogWrite(enableB, speed);
+
+  analogWrite(IN1, 180);
+  analogWrite(IN2, 0);
+  analogWrite(IN3, 180);
+  analogWrite(IN4, 0);
+
+  delay(3000);
+
+  analogWrite(IN1, 0);
+  analogWrite(IN2, 180);
+  analogWrite(IN3, 0);
+  analogWrite(IN4, 180);
+
+  delay(3000);
+
+}
