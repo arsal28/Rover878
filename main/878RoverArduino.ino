fff5ef82b35f1291d9eb0cd11b7776661a2a1fab
Arsal Abrar <aarsal359@gmail.com>
2024-01-20 22:58:29 -0500

    Starting code for line-following

--- main/878RoverArduino.ino
+++ main/878RoverArduino.ino
@@ -1,5 +1,8 @@
-const int analogIpt1 = A0; // infrared input left side
-const int speed = 255; 
+const int IN_A0 = A0;  // infrared input left side
+const int IN_A1 = A1;  // infrared input RIGHT side
+const int threshold = 500;
+
+const int speed = 255;
 
 // Motor A pins
 const int enableA = 11;
@@ -10,6 +13,8 @@ const int IN2 = 8;
 const int enableB = 10;
 const int IN3 = 7;
 const int IN4 = 6;
+
+void controlDir();
 
 void setup() {
   // Initialize pin mode
@@ -22,43 +27,40 @@ void setup() {
   pinMode(IN4, OUTPUT);
 
   // Turn off motors - Initial state
-	digitalWrite(IN1, LOW);
-	digitalWrite(IN2, LOW);
-	digitalWrite(IN3, LOW);
-	digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 
   Serial.begin(9600);
 }
 
 int changeVal;
 
-
-void controlDir(){
-  analogWrite(enableA, speed);
-  analogWrite(enableB, speed);
-
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-
-  delay(2000);
-
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-
-  delay(2000);
-}
-
 void loop() {
-  changeVal = map(analogRead(analogIpt1), 0, 1023, 0, 100);
-  Serial.print("Detected value: "+ changeVal);
+  value_A0 = analogRead(IN_A0);
+  value_A1 = analogRead(IN_A1);
+
+  changeVal = map(analogRead(IN_A0), 0, 1023, 0, 100);
+  Serial.print("Detected value: " + changeVal);
 
   analogWrite(enableA, speed);
   analogWrite(enableB, speed);
+
+  // **DONT TOUCH THIS CODE**
+  // if (value_A0 >= threshold) {
+  //   analogWrite(IN1, 0);
+  //   analogWrite(IN2, 0);
+  //   analogWrite(IN3, 0);
+  //   analogWrite(IN4, 0);
+  // }
+
+  // if (value_A1 >= threshold) {
+  //   analogWrite(IN1, 0);
+  //   analogWrite(IN2, 0);
+  //   analogWrite(IN3, 0);
+  //   analogWrite(IN4, 0);
+  // } 
 
   analogWrite(IN1, 180);
   analogWrite(IN2, 0);
@@ -73,5 +75,23 @@ void loop() {
   analogWrite(IN4, 180);
 
   delay(3000);
+}
 
+void controlDir() {
+  analogWrite(enableA, speed);
+  analogWrite(enableB, speed);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  delay(2000);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  delay(2000);
 }
