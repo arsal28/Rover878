ca04355215b798ef865faa3eb8f0229f1b84f477
Arsal Abrar <aarsal359@gmail.com>
2024-01-21 12:50:10 -0500

    merge

---
+++ main/rover_l298n/rover_l298n.ino
@@ -0,0 +1,76 @@
+#include <L298NX2.h>
+
+const int IN_A0 = A0;  // infrared input LEFT side
+const int IN_A1 = A1;  // infrared input RIGHT side
+
+const int trigPin = 12;
+const int echoPin = 13;
+const int IRThreshold = 20;
+const int USThreshold = 20;
+
+const int speed = 1000;
+
+float reading;
+
+L298NX2 motors(IN1_A, IN2_A, IN1_B, IN2_B);
+
+void controlDir();
+
+void setup() {
+  // Initialize pin mode
+  pinMode(IN_A0, INPUT);
+  pinMode(IN_A1, INPUT);
+  pinMode(trigPin, INPUT);
+
+  motors.setSpeed(10);
+
+  Serial.begin(9600);
+}
+
+int changeVal;
+
+void ultrasonicReading(){
+  pinMode(echoPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, LOW);
+  float duration = pulseIn(echoPin, HIGH);
+  float distance = (duration / 58);
+  reading = distance;
+}
+
+void loop() {
+  int IR_A0 = analogRead(IN_A0);
+  int IR_A1 = analogRead(IN_A1);
+
+  int changeValA0 = map(IR_A0, 0, 1023, 0, 100);
+  int changeValA1 = map(IR_A1, 0, 1023, 0, 100);
+  Serial.print(changeValA0);
+  Serial.print(" ");
+  Serial.print(reading);
+  Serial.print(" ");
+  Serial.println(changeValA1);
+
+  motors.backwardA();
+  motors.backwardB();
+
+  // if (IR_A0 >= IRThreshold) {
+  //   motors.stopB(); // Stops left motor
+  //   motors.forwardA(); // Move right
+  // }
+
+  // if (IR_A1 >= IRThreshold) {
+  //   motors.stopA(); // Stops right motor
+  //   motors.forwardB(); // Move left
+  // }
+}
+
+void moveLeft(){
+  motors.stopA();
+}
+
+void moveRight(){
+  motors.stopB();
+}
